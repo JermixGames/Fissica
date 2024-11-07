30af7ee72add286894efac2dc81d7e7423a9e6bc
Jeremy Castillo <118943962+JermixGames@users.noreply.github.com>
2024-11-07 00:28:44 -0500

    se agrego la sticky Bomb
    la cosa as kaboom

--- Assets/Scripts/StickyObject.cs
+++ Assets/Scripts/StickyObject.cs
@@ -1,6 +1,18 @@
+// StickyObject.cs
 using UnityEngine;
 
-public class StickyObject
+public class StickyObject : MonoBehaviour
 {
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Al colisionar, fija el objeto al otro objeto
+        transform.parent = collision.transform;
+
+        // Desactivar el Rigidbody para que no se mueva más
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+        }
+    }
 }
